c8d1ed3101f608e9887359a509378046fc86afac
Victor999069 <victorhugomiranda2013@gmail.com>
2023-08-16 22:13:58 -0300

    Modificações no registro animal e login.

--- src/pages/account/login/Login.jsx
+++ src/pages/account/login/Login.jsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
+import BaseLayout from "../../../template/layout/BaseLayout";
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handleSenhaChange = (event) => {
+        setSenha(event.target.value);
+    };
+
     return(
-        <h1>Login</h1>
+        <BaseLayout>
+            <div className="container-email">
+                <label htmlFor="email">E-mail:</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                />
+            </div>
+            <div className="container-senha">
+                <label htmlFor="senha">Senha:</label>
+                <input
+                    type="text"
+                    id="senha"
+                    value={senha}
+                    onChange={handleSenhaChange}
+                    required
+                />
+            </div>
+        </BaseLayout>
     );
 };
 
