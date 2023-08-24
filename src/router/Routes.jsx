9f0d9aa3b0ce448b618748025df089374c4f9771
Victor999069 <victorhugomiranda2013@gmail.com>
2023-08-24 19:53:41 -0300

    Atualização

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -9,33 +9,6 @@ import Register from "../pages/account/register/Register";
 import PasswordRecover from "../pages/account/password_recover/PasswordRecover";
 import Login from "../pages/account/login/Login";
 import Budget from "../pages/budget/Budget";
-<<<<<<< HEAD
-/*import Register_animal from "../pages/account/register_animal/Register_animal";*/
-
-const routes = createBrowserRouter([
-    {
-      path: '/',
-      element: <BaseLayout />,
-      children: [
-        { path: '/', element: <Home /> },
-        { path: '/home', element: <Home /> },
-        { path: 'contato', element: <Contact /> },
-        { path: 'sobre', element: <About /> },
-        { path: 'orcamento', element: <Budget /> }
-      ]
-    },
-    {
-      path: '/conta/',
-      element: <AccountLayout />,
-      children: [
-        { path: 'login', element: <Login /> },
-        { path: 'cadastro', element: <Register /> },
-        { path: 'perfil', element: <Profile /> },
-        { path: 'recuperar-senha', element: <PasswordRecover /> },
-      ]
-    },
-    { path: '*', element: <NotFound /> }
-=======
 
 const routes = createBrowserRouter([
   {
@@ -60,7 +33,6 @@ const routes = createBrowserRouter([
     ]
   },
   { path: '*', element: <NotFound /> }
->>>>>>> 300d1d3fe186d7a72f9a3c44ef2c1f9938a06224
 ]);
 
 export default routes;
