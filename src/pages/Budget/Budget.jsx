a237bf35c88ab282312d29d6b98430a6525f4aae
Victor999069 <victorhugomiranda2013@gmail.com>
2023-09-05 00:10:50 -0300

    Realizado melhorias e adionado novos comandos.

--- src/pages/Budget/Budget.jsx
+++ src/pages/Budget/Budget.jsx
@@ -1,6 +1,106 @@
+import { useRef, useState } from "react";
+
+
 const Budget = () => {
+    const idorçamentoRef = useRef('');
+    const serviçoRef = useRef('');
+    const precoservicoRef = useRef('');
+    const [totalservico, setTotalServico] = useState('');
+    const nomecliente = useRef('');
+    const telefonecliente = useRef('');
+    const emailclienteRef = useRef('');
+    const datavalidadeorca = useRef('');
+    const statusorca = useRef('');
+
+    const handleTotalServico = (event) => {
+        setTotalServico(event.target.value);
+    };
+
     return(
-        <h1>Orçamento</h1>
+        <div className="container">
+            <div>
+                <label htmlFor="idorçamentoRef"> Nº Orçamento:</label>
+                <input
+                    type="text"
+                    id="idorçamentoRef"
+                    ref={idorçamentoRef}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="serviçoRef">Tipo de serviço:</label>
+                <input
+                    type="text"
+                    id="serviçoRef"
+                    ref={serviçoRef}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="precoservicoRef">Preço serviço:</label>
+                <input
+                    type="text"
+                    id="precoservicoRef"
+                    ref={precoservicoRef}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="totalservico">Total serviço</label>
+                <input
+                    type="text"
+                    id="totalservico"
+                    value={totalservico}
+                    onChange={handleTotalServico}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="nomecliente">Nome cliente:</label>
+                <input
+                    type="text"
+                    id="nomecliente"
+                    ref={nomecliente}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="telefonecliente">Telefone Cliente:</label>
+                <input
+                    type="tel"
+                    id="telefonecliente"
+                    ref={telefonecliente}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="emailclienteRef">E-mail cliente:</label>
+                <input
+                    type="email"
+                    id="emailclienteRef"
+                    ref={emailclienteRef}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="datavalidadeorca">Validade orçamento:</label>
+                <input
+                    type="date"
+                    id="datavalidadeorca"
+                    ref={datavalidadeorca}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="statusorca">Status orçamento:</label>
+                <input
+                    type="text"
+                    id="statusorca"
+                    ref={statusorca}
+                    required
+                />
+            </div>
+        </div>
     );
 };
 
--- src/pages/account/register_animal/AnimalRegister.jsx
+++ src/pages/account/register_animal/AnimalRegister.jsx
@@ -1,51 +1,22 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 const AnimalRegister = () => {
-    const [idanimal, setIdAnimal] = useState('');
-    const [nomeanimal, setNomeAnimal] = useState('');
-    const [particularidade, setParticularidade] = useState('');
-    const [cartaovacina, setCartaoVacina] = useState('');
-    const [idade, setIdade] = useState('');
-    const [raca, setRaca] = useState('');
-    const [sexo, setSexo] = useState('');
-
-    const handleIdAnimalChange = (event) => {
-        setIdAnimal(event.target.value);
-    };
-
-    const handleNomeAnimalChange = (event) => {
-        setNomeAnimal(event.target.value);
-    };
-
-    const handleParticularidadeChange = (event) => {
-        setParticularidade(event.target.value);
-    };
-
-    const handleCartaoVacinaChange = (event) => {
-        setCartaoVacina(event.target.value);
-    };
-
-    const handleIdadeChange = (event) => {
-        setIdade(event.target.value);
-    };
-
-    const handleRacaChange = (event) => {
-        setRaca(event.target.value);
-    };
-
-    const handleSexoChange = (event) => {
-        setSexo(event.target.value);
-    };
+    const idanimalRef = useRef(''); 
+    const nomeanimalRef = useRef('');
+    const particularidadeRef = useRef('');
+    const cartaovacinaRef = useRef('');
+    const idadeRef = useRef('');
+    const racaRef = useRef('');
+    const sexoRef = useRef('');
 
     return(
-        <>
+        <div>
             <div className="container-id">
                 <label htmlFor="idanimal">Id Animal:</label>
                 <input
                     type="text"
                     id="idanimal"
-                    value={idanimal}
-                    onChange={handleIdAnimalChange}
+                    ref={idanimalRef}
                     required
                 />
             </div>
@@ -54,8 +25,7 @@ const AnimalRegister = () => {
                 <input
                     type="text"
                     id="nomeanimal"
-                    value={nomeanimal}
-                    onChange={handleNomeAnimalChange}
+                    ref={nomeanimalRef}
                     required
                 />
             </div>
@@ -64,8 +34,7 @@ const AnimalRegister = () => {
                 <input
                     type="text"
                     id="particularidade"
-                    value={particularidade}
-                    onChange={handleParticularidadeChange}
+                    ref={particularidadeRef}
                     required
                 />
             </div>
@@ -74,8 +43,7 @@ const AnimalRegister = () => {
                 <input
                     type="text"
                     id="cartaovacina"
-                    value={cartaovacina}
-                    onChange={handleCartaoVacinaChange}
+                    ref={cartaovacinaRef}
                     required
                 />
             </div>
@@ -84,8 +52,7 @@ const AnimalRegister = () => {
                 <input
                     type="text"
                     id="idade"
-                    value={idade}
-                    onChange={handleIdadeChange}
+                    ref={idadeRef}
                     required
                 />
             </div>
@@ -94,8 +61,7 @@ const AnimalRegister = () => {
                 <input
                     type="text"
                     id="raca"
-                    value={raca}
-                    onChange={handleRacaChange}
+                    ref={racaRef}
                     required
                 />
             </div>
@@ -104,12 +70,11 @@ const AnimalRegister = () => {
                 <input
                     type="text"
                     id="sexo"
-                    value={sexo}
-                    onChange={handleSexoChange}
+                    value={sexoRef}
                     required
                 />
             </div>
-        </>
+        </div>
     );
 };
 
